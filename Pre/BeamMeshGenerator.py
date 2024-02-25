713f7677b7b3b94662af0f61233d0d5d1a30c19a
@christian.oliveira1 <christian.oliveira@lccv.ufal.br>
2024-02-25 08:12:03 -0300

    fix: Ajustar a main para rodar o exemplo corretamente

--- Pre/BeamMeshGenerator.py
+++ Pre/BeamMeshGenerator.py
@@ -7,9 +7,15 @@ class BeamMeshGenerator:
     def __init__(self, geometry: Geometry):
         self.geometry = geometry
 
-    def generate_bilinear_mesh(self, width: float, height: float, num_x_nodes: int, num_y_nodes: int, x_origin: Optional[float] = 0., y_origin: Optional[float] = 0.):
-        x_spacing = width / (num_x_nodes - 1)
-        y_spacing = height / (num_y_nodes - 1)
+    from typing import Optional
+
+    def generate_bilinear_mesh(self, width: float, height: float, num_elements_x: int, num_elements_y: int,
+                               x_origin: Optional[float] = 0., y_origin: Optional[float] = 0.):
+        num_x_nodes = num_elements_x + 1
+        num_y_nodes = num_elements_y + 1
+
+        x_spacing = width / num_elements_x
+        y_spacing = height / num_elements_y
 
         for i in range(num_x_nodes):
             for j in range(num_y_nodes):
@@ -17,8 +23,8 @@ class BeamMeshGenerator:
                 y = y_origin + j * y_spacing
                 self.geometry.add_node(x, y)
 
-        for i in range(num_x_nodes - 1):
-            for j in range(num_y_nodes - 1):
+        for i in range(num_elements_x):
+            for j in range(num_elements_y):
                 n1 = j + i * num_y_nodes
                 n2 = j + (i + 1) * num_y_nodes
                 n3 = n1 + 1
--- main.py
+++ main.py
@@ -9,23 +9,23 @@ if __name__ == '__main__':
     element_type, geometry, nodes_restrictions, nodes_forces, elements_material = examples_reader.read_json_file('Examples/9_49_bana.json')
 
     if element_type == 'quad4':
-        BeamMeshGenerator(runner.geometry).generate_bilinear_mesh(width=geometry['width'], height=geometry['height'], num_x_nodes=geometry['num_nodes']['x'], num_y_nodes=geometry['num_nodes']['y'], x_origin=geometry['origin']['x'], y_origin=geometry['origin']['y'])
+        BeamMeshGenerator(runner.geometry).generate_bilinear_mesh(width=geometry['width'], height=geometry['height'], num_elements_x=geometry['num_nodes']['x'], num_elements_y=geometry['num_nodes']['y'], x_origin=geometry['origin']['x'], y_origin=geometry['origin']['y'])
 
     # Setting the material
     linear_elastic_material = runner.set_linear_elastic_material(young_modulus=elements_material['E'], poisson_ratio=elements_material['poisson'], thickness=elements_material['thickness'])
 
     for node in runner.geometry.nodes:
-
         for restriction_data in nodes_restrictions:
             pos_x = restriction_data['position']['x']
             pos_y = restriction_data['position']['y']
             if (pos_x is None or pos_x == node.x) and (pos_y is None or pos_y == node.y):
-                runner.set_boundary_conditions(node.label - 1, restriction_data['restrictions']['x'], restriction_data['restrictions']['y'])
+                runner.set_boundary_conditions(node.label - 1, restriction_data['restrictions']['x'],
+                                               restriction_data['restrictions']['y'])
 
         for force_data in nodes_forces:
             pos_x = force_data['position']['x']
             pos_y = force_data['position']['y']
-            if (pos_x == node.y) and (pos_y == node.y):
+            if (pos_x == node.x) and (pos_y == node.y):
                 runner.apply_nodal_load(node.label - 1, force_data['forces']['x'], force_data['forces']['y'])
 
 

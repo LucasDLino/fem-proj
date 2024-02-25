fix: Fixing stress/strain computation in Q8 reduced and visualization of constrained nodes. Also added flag to choose between full or reduced integration

Engine/Elements/BilinearQuadElement.py
@@ -25,6 +25,14 @@ class BilinearQuadElement(Element):
         else:
             raise ValueError('Material is not a LinearElasticMaterial')
 
+    def get_number_gp(self, stiff_intgr_type: str) -> int:
+        if stiff_intgr_type == 'full':
+            return 2
+        elif stiff_intgr_type == 'reduced':
+            return 1
+        else:
+            raise ValueError('stiff_intgr_type must be either "full" or "reduced"')
+
     def count_elem_dofs(self) -> int:
         return len(self.nodes) * 2
 
@@ -44,7 +52,9 @@ class BilinearQuadElement(Element):
 
         return derivative @ np.array([x, y]).T
 
-    def compute_elem_stiffness_matrix(self, number_gp: int) -> np.ndarray:
+    def compute_elem_stiffness_matrix(self, stiff_intgr_type: str) -> np.ndarray:
+        number_gp = self.get_number_gp(stiff_intgr_type)
+
         stiffness = np.zeros((self.count_elem_dofs(), self.count_elem_dofs()))
 
         weights = self.gauss.get_weights(number_gp)
@@ -75,8 +85,10 @@ class BilinearQuadElement(Element):
                 stiffness += jacobian_determinant * thickness * wi * wj * (b_matrix.T @ elastic_matrix @ b_matrix)
         return stiffness
 
-    def compute_stress_strain(self, global_displacement_vector: np.ndarray, number_gp: int) -> (np.ndarray, np.ndarray):
+    def compute_stress_strain(self, global_displacement_vector: np.ndarray, stress_strain_intgr_type: str) -> (np.ndarray, np.ndarray):
         """Implementation in BilinearQuadElement to compute stress and strain at the gauss points and nodes."""
+
+        number_gp = self.get_number_gp(stress_strain_intgr_type)
 
         self.stress_gp = []
         self.strain_gp = []
@@ -138,8 +150,10 @@ class BilinearQuadElement(Element):
 
         return extrapolation_matrix
 
-    def extrapolate_stress_strain_gp_to_nodes(self, number_gp: int):
+    def extrapolate_stress_strain_gp_to_nodes(self, stress_strain_intgr_type: str):
         """Extrapolates the stress or strain from the gauss points to the nodes."""
+
+        number_gp = self.get_number_gp(stress_strain_intgr_type)
 
         if number_gp == 2:  # Each row corresponds to a gauss point and each column to a node
             extrapolation_matrix = self.construct_extrapolation_matrix_2gp()
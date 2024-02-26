chore: Fixing Q9 shape functions and testing different computation order

Engine/Elements/QuadraticQuadElement.py
@@ -46,52 +46,85 @@ class QuadraticQuadElement(Element):
                                 [-(1. - xi) * (1. + eta) * (1. + xi - eta)],  # N7 (top left)
                                 [(1. - xi) * (1. - eta ** 2.) * 2.]])  # N8 (center left)
 
-    ''' linha a linha
-    def shape_functions_q9(self, xi: float, eta: float) -> np.ndarray: # linha por linha
-        return 0.25 * np.array([[-(1. - xi) * (1. - eta) * (1. + xi + eta)],  # N1 (bottom left)
-                                [(1. - xi ** 2.) * (1. - eta) * 2.],  # N2 (bottom center)
-                                [-(1. + xi) * (1. - eta) * (1. - xi + eta)],  # N3 (bottom right)
-                                [(1. - xi) * (1. - eta ** 2.) * 2.],  # N4 (center left)
-                                [4. * (1 - xi**2) * (1 - eta**2)],  # N5 (center center)
-                                [(1. + xi) * (1. - eta ** 2.) * 2.],  # N6 (center right)
-                                [-(1. - xi) * (1. + eta) * (1. + xi - eta)],  # N7 (top left)
-                                [(1. - xi ** 2.) * (1. + eta) * 2.],  # N8 (top center)
-                                [-(1. + xi) * (1. + eta) * (1. - xi - eta)]])  # N9 (top right)
-    '''
-    ''' espiral
-    def shape_functions_q9(self, xi: float, eta: float) -> np.ndarray:
-        return 0.25 * np.array([[-(1. - xi) * (1. - eta) * (1. + xi + eta)],  # N1 (bottom left)
-                                [(1. - xi ** 2.) * (1. - eta) * 2.],  # N2 (bottom center)
-                                [-(1. + xi) * (1. - eta) * (1. - xi + eta)],  # N3 (bottom right)
-                                [(1. + xi) * (1. - eta ** 2.) * 2.],  # N4 (center right)
-                                [-(1. + xi) * (1. + eta) * (1. - xi - eta)],  # N5 (top right)
-                                [(1. - xi ** 2.) * (1. + eta) * 2.],  # N6 (top center)
-                                [-(1. - xi) * (1. + eta) * (1. + xi - eta)],  # N7 (top left)
-                                [(1. - xi) * (1. - eta ** 2.) * 2.],  # N8 (center left)
-                                [4. * (1 - xi**2) * (1 - eta**2)]])  # N9 (center center)
-    '''
+    # Line by line
+    # def shape_functions_q9(self, xi: float, eta: float) -> np.ndarray:
+    #     return 0.25 * np.array([[(1 - xi) * (1 - eta) * xi * eta],  # N1 (bottom left)
+    #                             [-2 * (1 - xi ** 2) * (1 - eta) * eta],  # N2 (bottom center)
+    #                             [-(1 + xi) * (1 - eta) * xi * eta],  # N3 (bottom right)
+    #                             [-2 * (1 - xi) * (1 - eta ** 2) * xi],  # N4 (center left)
+    #                             [(1 - xi ** 2) * (1 - eta ** 2)],  # N5 (center center)
+    #                             [2 * (1 + xi) * (1 - eta ** 2) * xi],  # N6 (center right)
+    #                             [-(1 - xi) * (1 + eta) * xi * eta],  # N7 (top left)
+    #                             [2 * (1 - xi ** 2) * (1 + eta) * eta],  # N8 (top center)
+    #                             [(1 + xi) * (1 + eta) * xi * eta]])  # N9 (top right)
 
-    ''' doidona '''
+    # Literature version - Books style
+    # def shape_functions_q9(self, xi: float, eta: float) -> np.ndarray:
+    #     return 0.25 * np.array([[xi * (xi - 1) * eta * (eta - 1)],  # N1 (bottom left)
+    #                             [xi * (xi + 1) * eta * (eta - 1)],  # N3 (bottom right)
+    #                             [xi * (xi + 1) * eta * (eta + 1)],  # N9 (top right)
+    #                             [xi * (xi - 1) * eta * (eta + 1)],  # N7 (top left)
+    #                             [-2 * (xi + 1) * (xi - 1) * eta * (eta - 1)],  # N2 (bottom center)
+    #                             [-2 * xi * (xi + 1) * (eta + 1) * (eta - 1)],  # N6 (center right)
+    #                             [-2 * (xi + 1) * (xi - 1) * eta * (eta + 1)],  # N8 (top center)
+    #                             [-2 * xi * (xi - 1) * (eta + 1) * (eta - 1)],  # N4 (center left)
+    #                             [(xi + 1) * (xi - 1) * (eta + 1) * (eta - 1)]
+    #                             ])  # N5 (center center)
+
+    # Literature version
+    # def shape_functions_q9(self, xi: float, eta: float) -> np.ndarray:
+    #     return 0.25 * np.array([[(1 - xi) * (1 - eta) * xi * eta],  # N1 (bottom left)
+    #                             [-(1 + xi) * (1 - eta) * xi * eta],  # N3 (bottom right)
+    #                             [(1 + xi) * (1 + eta) * xi * eta],  # N9 (top right)
+    #                             [-(1 - xi) * (1 + eta) * xi * eta],  # N7 (top left)
+    #                             [-2 * (1 - xi ** 2) * (1 - eta) * eta],  # N2 (bottom center)
+    #                             [2 * (1 + xi) * (1 - eta ** 2) * xi],  # N6 (center right)
+    #                             [2 * (1 - xi ** 2) * (1 + eta) * eta],  # N8 (top center)
+    #                             [-2 * (1 - xi) * (1 - eta ** 2) * xi],  # N4 (center left)
+    #                             [(1 - xi ** 2) * (1 - eta ** 2)]])  # N5 (center center)
+
+    # Literature version 2
     def shape_functions_q9(self, xi: float, eta: float) -> np.ndarray:
-        return 0.25 * np.array([[-(1. - xi) * (1. - eta) * (1. + xi + eta)],  # N1 (bottom left)
-                                [-(1. + xi) * (1. - eta) * (1. - xi + eta)],  # N3 (bottom right)
-                                [-(1. + xi) * (1. + eta) * (1. - xi - eta)],  # N5 (top right)
-                                [-(1. - xi) * (1. + eta) * (1. + xi - eta)],  # N7 (top left)
-                                [(1. - xi ** 2.) * (1. - eta) * 2.],  # N2 (bottom center)
-                                [(1. + xi) * (1. - eta ** 2.) * 2.],  # N4 (center right)
-                                [(1. - xi ** 2.) * (1. + eta) * 2.],  # N6 (top center)
-                                [(1. - xi) * (1. - eta ** 2.) * 2.],  # N8 (center left)
-                                [4. * (1 - xi**2) * (1 - eta**2)]])  # N9 (center center)
-    ''''''
+        return 0.25 * np.array([[(1 + xi) * (1 + eta) * xi * eta],  # N9 (top right)
+                                [-(1 - xi) * (1 + eta) * xi * eta],  # N7 (top left)
+                                [(1 - xi) * (1 - eta) * xi * eta],  # N1 (bottom left)
+                                [-(1 + xi) * (1 - eta) * xi * eta],  # N3 (bottom right)
+                                [2 * (1 - xi ** 2) * (1 + eta) * eta],  # N8 (top center)
+                                [-2 * (1 - xi) * (1 - eta ** 2) * xi],  # N4 (center left)
+                                [-2 * (1 - xi ** 2) * (1 - eta) * eta],  # N2 (bottom center)
+                                [2 * (1 + xi) * (1 - eta ** 2) * xi],  # N6 (center right)
+                                [(1 - xi ** 2) * (1 - eta ** 2)]])  # N5 (center center)
+
+    # Spiral version
+    # def shape_functions_q9(self, xi: float, eta: float) -> np.ndarray:
+    #     return 0.25 * np.array([[(1 - xi) * (1 - eta) * xi * eta],  # N1 (bottom left)
+    #                             [-2 * (1 - xi ** 2) * (1 - eta) * eta],  # N2 (bottom center)
+    #                             [-(1 + xi) * (1 - eta) * xi * eta],  # N3 (bottom right)
+    #                             [2 * (1 + xi) * (1 - eta ** 2) * xi],  # N6 (center right)
+    #                             [(1 + xi) * (1 + eta) * xi * eta],  # N9 (top right)
+    #                             [2 * (1 - xi ** 2) * (1 + eta) * eta],  # N8 (top center)
+    #                             [-(1 - xi) * (1 + eta) * xi * eta],  # N7 (top left)
+    #                             [-2 * (1 - xi) * (1 - eta ** 2) * xi],  # N4 (center left)
+    #                             [(1 - xi ** 2) * (1 - eta ** 2)]])  # N5 (center center)
+
+    # Column by column
+    # def shape_functions_q9(self, xi: float, eta: float) -> np.ndarray:
+    #     return 0.25 * np.array([[(1 - xi) * (1 - eta) * xi * eta],  # N1 (bottom left)
+    #                             [-2 * (1 - xi) * (1 - eta ** 2) * xi],  # N4 (center left)
+    #                             [-(1 - xi) * (1 + eta) * xi * eta],  # N7 (top left)
+    #                             [-2 * (1 - xi ** 2) * (1 - eta) * eta],  # N2 (bottom center)
+    #                             [(1 - xi ** 2) * (1 - eta ** 2)],  # N5 (center center)
+    #                             [2 * (1 - xi ** 2) * (1 + eta) * eta],  # N8 (top center)
+    #                             [-(1 + xi) * (1 - eta) * xi * eta],  # N3 (bottom right)
+    #                             [2 * (1 + xi) * (1 - eta ** 2) * xi],  # N6 (center right)
+    #                             [(1 + xi) * (1 + eta) * xi * eta],  # N9 (top right)
+    #                             ])
 
     def linear_shape_functions(self, xi: float, eta: float) -> np.ndarray:
         return 0.25 * np.array([[(1. - xi) * (1. - eta)],  # N1 (bottom left)
                                 [(1. + xi) * (1. - eta)],  # N2 (bottom right)
                                 [(1. + xi) * (1. + eta)],  # N3 (top right)
                                 [(1. - xi) * (1. + eta)]])  # N4 (top left)
-
-
-
 
     def shape_functions_derivative(self, xi: float, eta: float) -> np.ndarray:
         # Derivadas parciais em relação a xi e eta organizadas em uma matriz
@@ -218,16 +251,16 @@ class QuadraticQuadElement(Element):
 
         return extrapolation_matrix
 
-
-
     def construct_extrapolation_matrix_3GP(self) -> np.ndarray:
         """Constructs the extrapolation matrix for the element."""
         num_nodes = len(self.nodes)
 
         # Construct parametric coordinates
-        parametric_coords = np.array([[-1, -1], [1, -1], [1, 1], [-1, 1], [0, -1], [1, 0], [0, 1], [-1, 0]])  # forma doidona
-        # parametric_coords = np.array([[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]])  # forma linha por linha
-        # parametric_coords = np.array([[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]]) # forma em espiral
+        # parametric_coords = np.array([[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]])  # Line by line
+        # parametric_coords = np.array([[-1, -1], [1, -1], [1, 1], [-1, 1], [0, -1], [1, 0], [0, 1], [-1, 0]])  # Literature version
+        # parametric_coords = np.array([[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]])  # Spiral
+        parametric_coords = np.array([[1, -1], [-1, 1], [-1, -1], [1, -1], [0, 1], [-1, 0], [0, -1], [1, 0]])  # Literature version 2
+        # parametric_coords = np.array([[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]) # Column by column
 
         num_cols = 9
 
@@ -238,8 +271,7 @@ class QuadraticQuadElement(Element):
             xi = parametric_coords[i, 0]
             eta = parametric_coords[i, 1]
 
-            # N_i = 0.25 * (1 +- xi') * (1 +- eta') in which xi' = xi * sqrt(3) and eta' = eta * sqrt(3)
-            shape_funcs = self.shape_functions_q9(xi * (1 / np.sqrt(0.6)), eta * (1 / np.sqrt(0.6)))
+            shape_funcs = self.shape_functions_q9(xi * 1 / (np.sqrt(0.6)), eta * 1 / (np.sqrt(0.6)))
 
             extrapolation_matrix[i, :] = shape_funcs.ravel()
 
@@ -251,12 +283,15 @@ class QuadraticQuadElement(Element):
         number_gp = self.get_number_gp(stress_strain_intgr_type)
 
         if number_gp == 2:
-            # Each row corresponds to a gauss point and each column to a node
+            # Each row corresponds to all shape functions evaluated at a gauss point and each column to gauss point (i.e., specific shape function)
             extrapolation_matrix = self.construct_extrapolation_matrix_2GP()
         elif number_gp == 3:
             extrapolation_matrix = self.construct_extrapolation_matrix_3GP()
         else:
             raise ValueError('Number of gauss points not supported')
+
+        # Check sum of all columns of each row
+        check_ones = np.sum(extrapolation_matrix, axis=1)
 
         # Extrapolate the stress and strain from the gauss points to the nodes
         strain_xx = extrapolation_matrix @ self.strain_gp[:, 0]
@@ -272,6 +307,3 @@ class QuadraticQuadElement(Element):
             # node.strain corresponds to a list of lists of type [[elem_label, (nparray) strain], ...]
             node.strain.append([self.label, np.array([strain_xx[i], strain_yy[i], strain_xy[i]])])
             node.stress.append([self.label, np.array([stress_xx[i], stress_yy[i], stress_xy[i]])])
-
-
-

Engine/Gauss.py
@@ -145,49 +145,71 @@ class Gauss(object):
             min_xi, max_xi = min(gauss_points), max(gauss_points)
             min_eta, max_eta = min(gauss_points), max(gauss_points)
             central_point = gauss_points[1]
-            ''' linha'''
-            # Form arranged_gauss_points based on the min and max values
-            arranged_gauss_points = np.array([ # linha a linha
-                [min_xi, min_eta],
-                [central_point, min_eta],
-                [max_xi, min_eta],
-                [min_xi, central_point],
-                [central_point, central_point],
-                [max_xi, central_point],
-                [min_xi, max_eta],
-                [central_point, max_eta],
-                [max_xi, max_eta]
-            ])
-            ''''''
 
-            ''' espiral
+            # Line by line
+            # arranged_gauss_points = np.array([
+            #     [min_xi, min_eta],
+            #     [central_point, min_eta],
+            #     [max_xi, min_eta],
+            #     [min_xi, central_point],
+            #     [central_point, central_point],
+            #     [max_xi, central_point],
+            #     [min_xi, max_eta],
+            #     [central_point, max_eta],
+            #     [max_xi, max_eta]
+            # ])
+
+            # Spiral version
+            # arranged_gauss_points = np.array([
+            #     [min_xi, min_eta],
+            #     [central_point, min_eta],
+            #     [max_xi, min_eta],
+            #     [max_xi, central_point],
+            #     [max_xi, max_eta],
+            #     [central_point, max_eta],
+            #     [min_xi, max_eta],
+            #     [min_xi, central_point],
+            #     [central_point, central_point]
+            # ])
+
+            # Literature version
+            # arranged_gauss_points = np.array([
+            #     [min_xi, min_eta],
+            #     [max_xi, min_eta],
+            #     [max_xi, max_eta],
+            #     [min_xi, max_eta],
+            #     [central_point, min_eta],
+            #     [max_xi, central_point],
+            #     [central_point, max_eta],
+            #     [min_xi, central_point],
+            #     [central_point, central_point]
+            # ])
+
+            # Literature version 2
             arranged_gauss_points = np.array([
-                [min_xi, min_eta],
-                [central_point, min_eta],
-                [max_xi, min_eta],
-                [max_xi, central_point],
-                [max_xi, max_eta],
-                [central_point, max_eta],
-                [min_xi, max_eta],
-                [min_xi, central_point],
-                [central_point, central_point]
-            ])
-            '''
-
-            '''
-            arranged_gauss_points = np.array([ #doido
-                [min_xi, min_eta],
-                [max_xi, min_eta],
                 [max_xi, max_eta],
                 [min_xi, max_eta],
-                [central_point, min_eta],
-                [max_xi, central_point],
+                [min_xi, min_eta],
+                [max_xi, min_eta],
                 [central_point, max_eta],
                 [min_xi, central_point],
+                [central_point, min_eta],
+                [max_xi, central_point],
                 [central_point, central_point]
             ])
-            '''
 
+            # Column by column
+            # arranged_gauss_points = np.array([
+            #     [min_xi, min_eta],
+            #     [min_xi, central_point],
+            #     [min_xi, max_eta],
+            #     [central_point, min_eta],
+            #     [central_point, central_point],
+            #     [central_point, max_eta],
+            #     [max_xi, min_eta],
+            #     [max_xi, central_point],
+            #     [max_xi, max_eta]
+            # ])
 
             return arranged_gauss_points
         else: